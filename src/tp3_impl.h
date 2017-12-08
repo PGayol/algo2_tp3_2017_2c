8e8f92dcd6b45c46319500ac3bdfc108431bf14c
Patricio <pgayol@dc.uba.ar>
2017-12-08 12:06:27 -0300

    achicado el array de billeters y arreglados sus indices

--- src/tp3_impl.h
+++ src/tp3_impl.h
@@ -135,16 +135,10 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
 {
     auto anioMax = anioMaximo(falsos_conocidos);
     auto anioMin = anioMinimo(falsos_conocidos);
-    int tam = anioMax - anioMin;
+    int tam = anioMax - anioMin + 1;
     // tam es el tamaño del vector de años
     fajo res = a_ordenar;
-    vector<std::set<billete> > anios(anioMax);
-
-    /*for(int i = 0; i < anioMax; i++)
-    {
-        anios[i] =
-
-    }*/
+    vector<std::set<billete> > anios(tam);
 
     for(int i = 0; i < falsos_conocidos.size(); i++ )
     {
@@ -152,7 +146,7 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
         std::set<billete> actual = anios[int(a_insertar)];
         actual.insert(actual.begin(), a_insertar);*/
         auto aInsertar = falsos_conocidos[i];
-        anios[int(aInsertar) -1].insert(/*anios[int(aInsertar)].begin(),*/ aInsertar);
+        anios[int(aInsertar) -1 - (anioMin-1)].insert(/*anios[int(aInsertar)].begin(),*/ aInsertar);
     }
 
     for( int j = 0; j < res.size(); j++) //recorro el fajo a ordenar para setear su probabilidad de que sea falso
@@ -163,8 +157,8 @@ fajo ordenar_por_probabilidad(const fajo& falsos_conocidos, const fajo & a_orden
         //auto & a_buscar = anios[int(a_setear)];
         //auto it = anios[int(res[j])-1].find(res[j]); // busco el billete en el año que fue creado find devuevle un it a la posicion del billete si no esta res end()
 
-        if(/*it != anios[int(res[j])-1].end()*/ anios[int(res[j])-1].count(res[j]) == 0) { // si no encuentro el billete le asigno su probabilidad de que sea falso
-            res[j].probabilidad_falso = anios[int(res[j])-1].size();//si no encuentro el billete en el set, su probabilidad de que sea falso es la cantidad de billetes falsos que el banco tiene en su listado para ese año
+        if(/*it != anios[int(res[j])-1].end()*/ anios[int(res[j])-1 - (anioMin-1)].count(res[j]) == 0) { // si no encuentro el billete le asigno su probabilidad de que sea falso
+            res[j].probabilidad_falso = anios[int(res[j])-1 - (anioMin-1)].size();//si no encuentro el billete en el set, su probabilidad de que sea falso es la cantidad de billetes falsos que el banco tiene en su listado para ese año
         }
 
     }
